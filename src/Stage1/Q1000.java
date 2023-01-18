750416c1f64bd0680b6459dcaf0daeba9e32abfc
Ki moon Park <p.rainmakerm@gmail.com>
2023-01-18 23:36:47 +0900

    BOJ - Q1000

---
+++ src/Stage1/Q1000.java
@@ -0,0 +1,15 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class Q1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}
--- src/Stage1/Q2557.java
+++ src/Stage1/Q2557.java
@@ -1,3 +1,5 @@
+package Stage1;
+
 public class Q2557 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
