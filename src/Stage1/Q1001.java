dc7215bd39382854c31d017f3a7434e67bd2d4e6
Ki moon Park <p.rainmakerm@gmail.com>
2023-01-23 20:43:50 +0900

    Q1001

---
+++ src/Stage1/Q1001.java
@@ -0,0 +1,17 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class Q1001 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a - b);
+    }
+}
